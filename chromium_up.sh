280bfea4e6a0157373a892f172a40bb3539648f8
Alexis Greppo <alexis.greppo@gmail.com>
2013-07-28 15:46:39 +0200

    New syntax for number version on SVN

--- chromium_up.sh
+++ chromium_up.sh
@@ -2,81 +2,47 @@
 # Créer par Alexis Greppo
 # Site : http://www.zen-tech.info
  
+# On récupère le numéro de la dernière révision disponible sur le SVN 
+SVN_REVISION=`curl -s https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/LAST_CHANGE`
+echo "Version SVN $SVN_REVISION"
+
+cd /tmp
  
-function get_sys_version {
+# Téléchargement de la nouvelle version
+curl -L -O "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/$SVN_REVISION/chrome-mac.zip"
  
-	# On récupère la version installée actuellement
+if [ $? -eq 0 ];then
+	unzip -q ./chrome-mac.zip
+fi
  
-	if [ -f "/Applications/Chromium.app/Contents/Info.plist" ];then
-		SYS_VERSION=`defaults read /Applications/Chromium.app/Contents/Info SVNRevision`
-		if [ $? -ne 0 ];then
-			SYS_VERSION=`defaults read /Applications/Chromium.app/Contents/Info SCMRevision`
-		fi
-	else
-		SYS_VERSION=0
+echo "Installation de l'application"
+ 
+if [ -f "/Applications/Chromium.app/Contents/Info.plist" ];then
+	mv /Applications/Chromium.app /Applications/Chromium_old.app
+fi
+ 
+cp -R ./chrome-mac/Chromium.app /Applications/
+ 
+if [ $? -eq 0 ];then
+	if [ -f "/Applications/Chromium_old.app/Contents/Info.plist" ];then
+		rm -r /Applications/Chromium_old.app
 	fi
+else
+	mv /Applications/Chromium_old.app /Applications/Chromium.app
+fi
  
-	echo "Version installee $SYS_VERSION"
+echo "Suppression des fichiers temporaires"
+rm -r ./chrome-mac
+rm chrome-mac.zip
  
-}
- 
-function get_svn_version {
-	# On récupère le numéro de la dernière révision disponible sur le SVN 
-	SVN_REVISION=`curl -s http://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/LAST_CHANGE`
-	echo "Version SVN $SVN_REVISION"
-}
- 
-function install_chromium {
- 
-	cd /tmp
- 
-	# Téléchargement de la nouvelle version
- 
-	curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/$1/chrome-mac.zip"
- 
-	if [ $? -eq 0 ];then
-		unzip -q ./chrome-mac.zip
-	fi
- 
-	echo "Installation de l'application"
- 
-	if [ -f "/Applications/Chromium.app/Contents/Info.plist" ];then
-		mv /Applications/Chromium.app /Applications/Chromium_old.app
-	fi
- 
-	cp -R ./chrome-mac/Chromium.app /Applications/
- 
-	if [ $? -eq 0 ];then
-		if [ -f "/Applications/Chromium_old.app/Contents/Info.plist" ];then
-			rm -r /Applications/Chromium_old.app
-		fi
-	else
-		mv /Applications/Chromium_old.app /Applications/Chromium.app
-	fi
- 
-	echo "Suppression des fichiers temporaires"
-	rm -r ./chrome-mac
-	rm chrome-mac.zip
- 
-}
- 
-# On récupère la version installee et la version en ligne
-get_sys_version
-get_svn_version
- 
-if [ $SYS_VERSION -lt $SVN_REVISION ];then
- 
-	ps aux | grep /Applications/Chromium.app/Contents/MacOS/Chromium | grep -v grep
-	# 1 si pas de ligne trouvée
-	# 0 si chromium lancé
-	# echo $?
- 
-	if [ $? -eq 1 ];then
-		install_chromium $SVN_REVISION
-	else
-		echo "Vous devez fermer Chromium avant de lancer la mise à jour"
-	fi
- 
-else 
-	echo "Pas de mise à jour disponible"
+# On vérifie si Chromium est en cours d'exécution
+ps aux | grep /Applications/Chromium.app/Contents/MacOS/Chromium | grep -v grep
+
+# 1 si pas de ligne trouvée
+# 0 si chromium lancé	
+
+if [ $? -eq 1 ];then
+	install_chromium $SVN_REVISION
+else
+	echo "Vous devez fermer Chromium avant de lancer la mise à jour"
 fi
