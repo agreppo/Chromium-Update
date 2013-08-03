5618ae5bbe351895edb0d7343c37489c773fce3b
Alexis Greppo <alexis.greppo@gmail.com>
2013-08-03 08:49:23 +0200

    Correction d'erreurs dans le script

--- chromium_up.sh
+++ chromium_up.sh
@@ -1,40 +1,7 @@
 # !/bin/sh
 # Créer par Alexis Greppo
 # Site : http://www.zen-tech.info
- 
-# On récupère le numéro de la dernière révision disponible sur le SVN 
-SVN_REVISION=`curl -s https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/LAST_CHANGE`
-echo "Version SVN $SVN_REVISION"
 
-cd /tmp
- 
-# Téléchargement de la nouvelle version
-curl -L -O "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/$SVN_REVISION/chrome-mac.zip"
- 
-if [ $? -eq 0 ];then
-	unzip -q ./chrome-mac.zip
-fi
- 
-echo "Installation de l'application"
- 
-if [ -f "/Applications/Chromium.app/Contents/Info.plist" ];then
-	mv /Applications/Chromium.app /Applications/Chromium_old.app
-fi
- 
-cp -R ./chrome-mac/Chromium.app /Applications/
- 
-if [ $? -eq 0 ];then
-	if [ -f "/Applications/Chromium_old.app/Contents/Info.plist" ];then
-		rm -r /Applications/Chromium_old.app
-	fi
-else
-	mv /Applications/Chromium_old.app /Applications/Chromium.app
-fi
- 
-echo "Suppression des fichiers temporaires"
-rm -r ./chrome-mac
-rm chrome-mac.zip
- 
 # On vérifie si Chromium est en cours d'exécution
 ps aux | grep /Applications/Chromium.app/Contents/MacOS/Chromium | grep -v grep
 
@@ -42,7 +9,38 @@ ps aux | grep /Applications/Chromium.app/Contents/MacOS/Chromium | grep -v grep
 # 0 si chromium lancé	
 
 if [ $? -eq 1 ];then
-	install_chromium $SVN_REVISION
+	# On récupère le numéro de la dernière révision disponible sur le SVN 
+	SVN_REVISION=`curl -s https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/LAST_CHANGE`
+	echo "Version SVN $SVN_REVISION"
+
+	cd /tmp
+ 
+	# Téléchargement de la nouvelle version
+	curl -L -O "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/$SVN_REVISION/chrome-mac.zip"
+ 
+	if [ $? -eq 0 ];then
+		unzip -q ./chrome-mac.zip
+	fi
+ 
+	echo "Installation de l'application"
+ 
+	if [ -f "/Applications/Chromium.app/Contents/Info.plist" ];then
+		mv /Applications/Chromium.app /Applications/Chromium_old.app
+	fi
+ 
+	cp -R ./chrome-mac/Chromium.app /Applications/
+ 
+	if [ $? -eq 0 ];then
+		if [ -f "/Applications/Chromium_old.app/Contents/Info.plist" ];then
+			rm -r /Applications/Chromium_old.app
+		fi
+	else
+		mv /Applications/Chromium_old.app /Applications/Chromium.app
+	fi
+ 
+	echo "Suppression des fichiers temporaires"
+	rm -r ./chrome-mac
+	rm chrome-mac.zip
 else
 	echo "Vous devez fermer Chromium avant de lancer la mise à jour"
 fi
